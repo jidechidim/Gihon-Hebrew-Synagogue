0d3ae4f593435576c1410fda41a4c6a689fd6183
Jidechi Dimunah <jidechidim@gmail.com>
2025-11-14 10:50:11 +0100

    Remove metadata from client layouts to fix Next.js build

--- app/(frontend)/layout.jsx
+++ app/(frontend)/layout.jsx
@@ -1,8 +1,11 @@
 // /app/(frontend)/layout.jsx
+"use client"; // if you need client features like hooks
+
 import "../globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+// REMOVE metadata export here
 export default function FrontendLayout({ children }) {
   return (
     <>
