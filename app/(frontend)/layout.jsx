233915a1df1609c10ab4f39899f22d92c74b1233
Jidechi Dimunah <jidechidim@gmail.com>
2025-11-14 10:26:46 +0100

    Fix layouts: separate root, frontend, and admin layouts

--- app/(frontend)/layout.jsx
+++ app/(frontend)/layout.jsx
@@ -1,10 +1,7 @@
+// /app/(frontend)/layout.jsx
 import "../globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-
-export const metadata = {
-  title: "Gihon Hebrew Synagogue",
-};
 
 export default function FrontendLayout({ children }) {
   return (
